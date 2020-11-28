27f7a3cb2885d14192822dc7bfb800288088a04e
Patrick Kerrigan <patrick@patrickkerrigan.uk>
2020-11-28 12:58:22 +0000

    Drop 7.0 support and upgrade PHPUnit

--- tests/TraceTest.php
+++ tests/TraceTest.php
@@ -54,11 +54,10 @@ class TraceTest extends TestCase
         $this->assertRegExp('@^1\-[a-f0-9]{8}\-[a-f0-9]{24}$@', $trace->getTraceId());
     }
 
-    /**
-     * @expectedException \TypeError
-     */
     public function testGivenNullHeaderDoesNotSetId()
     {
+        $this->expectException(\TypeError::class);
+
         $trace = new Trace();
         $trace->setTraceHeader(null);
 
